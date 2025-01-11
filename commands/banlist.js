c21af4e305c68bca510f38381ad6e161624ec798
GeoArchonMorax <korisawarezero@gmail.com>
2025-01-11 12:21:02 +0800

    Update banlist.js

--- commands/banlist.js
+++ commands/banlist.js
@@ -1,21 +1,19 @@
 module.exports = {
   name: 'banlist',
   category: 'Moderation',
-  execute: async (api, event, args, commands, prefix, admins, appState, sendMessage, globalData) => {
+  execute: async (api, event, args, commands, prefix, admins, appState, sendMessage) => {
     const { threadID, senderID } = event;
-    const _m = "banlist";
 
     if (!admins.includes(senderID)) {
       return sendMessage(api, { threadID, message: "You don't have permission to use this command." });
     }
 
-    let banlist = await globalData.get(_m) || { data: [] };
-    if (banlist.data.length === 0) {
+    if (!global.data.banlist || global.data.banlist.length === 0) {
       return sendMessage(api, { threadID, message: "There are no banned users." });
     }
 
     let message = "Banned Users 🚫\n";
-    for (const bannedUser of banlist.data) {
+    for (const bannedUser of global.data.banlist) {
       message += `- ${bannedUser.name} [${bannedUser.id}]\n`;
     }
 
