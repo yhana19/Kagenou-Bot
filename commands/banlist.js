ca2922ac82a98c94c081b85f87afd9e2e141c0f3
GeoArchonMorax <korisawarezero@gmail.com>
2025-01-11 12:14:19 +0800

    Update banlist.js

--- commands/banlist.js
+++ commands/banlist.js
@@ -1,13 +1,11 @@
-const { post, get } = require("axios"); 
-
 module.exports = {
   name: 'banlist',
   category: 'Moderation',
-  execute: async (api, event, args, commands, prefix, admins, appState, sendMessage) => {
+  execute: async (api, event, args, commands, prefix, admins, appState, sendMessage, globalData) => {
     const { threadID, senderID } = event;
-    const _m = "banlist"; 
+    const _m = "banlist";
 
-    if (!admins.includes(senderID)) { 
+    if (!admins.includes(senderID)) {
       return sendMessage(api, { threadID, message: "You don't have permission to use this command." });
     }
 
@@ -16,16 +14,9 @@ module.exports = {
       return sendMessage(api, { threadID, message: "There are no banned users." });
     }
 
-    let message = "Banned Users:\n";
-    for (const bannedId of banlist.data) {
-      try {
-        const userInfo = await api.getUserInfo(bannedId);
-        const bannedName = userInfo[bannedId].name;
-        message += `- ${bannedName} [${bannedId}]\n`;
-      } catch (error) {
-        console.error(`Error getting user info for ${bannedId}:`, error);
-        message += `- Unknown User [${bannedId}]\n`;
-      }
+    let message = "Banned Users 🚫\n";
+    for (const bannedUser of banlist.data) {
+      message += `- ${bannedUser.name} [${bannedUser.id}]\n`;
     }
 
     return sendMessage(api, { threadID, message: message });
