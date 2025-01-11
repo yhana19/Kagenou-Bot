38c19813a8fb6e9c56dd1722b1afb90cc9425afa
GeoArchonMorax <korisawarezero@gmail.com>
2025-01-11 16:32:29 +0800

    Update banlist.js

--- commands/banlist.js
+++ commands/banlist.js
@@ -1,36 +1,26 @@
 const fs = require('node:fs/promises');
-const { api } = require('./index'); // Assuming api is exported from index.js
+const path = require('path');
 
 module.exports = {
-  name: 'banlist',
-  category: 'Mod',
-    // ... (other parts of banlist.js remain the same)
-
+    name: 'banlist',
     execute: async (api, event, args, commands, prefix, admins, appState, sendMessage) => {
-        // ... (admin check remains the same)
+        if (!admins.includes(event.senderID)) {
+            return sendMessage(api, { threadID: event.threadID, message: 'You do not have permission to use this command.' });
+        }
 
         try {
-            const bannedUsers = JSON.parse(await fs.readFile('./banned.json', 'utf8')) || [];
-            let message = "Banned Users 🚫\n";
-            if (bannedUsers.length === 0) {
-                message = "There are no banned users.";
-            } else {
-                for (const userId of bannedUsers) {
-                    try {
-                        const userInfo = await api.getUserInfo(userId);
-                        const userName = userInfo[userId].name;
-                        message += `- ${userName} [${userId}]\n`;
-                    } catch (error) {
-                        console.error(`Error getting user info for ${userId}:`, error);
-                        message += `- Unknown User [${userId}]\n`;
-                    }
-                }
+            const banlistPath = path.join(__dirname, 'banlist.json');
+            const banlist = JSON.parse(await fs.readFile(banlistPath, 'utf8')) || [];
+
+            if (banlist.length === 0) {
+                return sendMessage(api, { threadID: event.threadID, message: '🚫 Banned list 🚫\nNo users are currently banned.' });
             }
-            return sendMessage(api, { threadID, message });
+
+            const banListMessage = banlist.map((user) => `- ${user.userName} (ID: ${user.userID})`).join('\n');
+            sendMessage(api, { threadID: event.threadID, message: `🚫 Banned list 🚫\n${banListMessage}` });
         } catch (error) {
-            console.error('Error getting ban list:', error);
-            return sendMessage(api, { threadID, message: 'Error getting ban list.' });
+            console.error('Error retrieving ban list:', error);
+            sendMessage(api, { threadID: event.threadID, message: 'Error retrieving ban list. Please check the console for details.' });
         }
-    }
+    },
 };
-                          
