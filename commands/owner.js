df65a3650ff17cb456db21c1893ae41fcf386d31
GeoArchonMorax <korisawarezero@gmail.com>
2025-01-12 16:56:01 +0800

    Update owner.js

--- commands/owner.js
+++ commands/owner.js
@@ -1,13 +1,9 @@
 const axios = require('axios');
-const fs = require('fs');
-const path = require('path');
-
 module.exports = {
   name: 'developer',
   category: 'Admin',
   execute: async (api, event, args, commands, prefix, admins, appState, sendMessage) => {
     const { threadID } = event;
-
     const developer = {
       name: 'Aljur Pogoy', 
       gfname: 'Ana Sophia',
@@ -15,29 +11,14 @@ module.exports = {
       age: 17,
       imageUrl: 'https://imgur.com/a/r7Vi23B.jpg'
     };
-
     const response = `Developer Information 🧾
 Name: ${developer.name}
 gfname: ${developer.gfname}
 height: ${developer.height}
 Age: ${developer.age}`;
-
     try {
-      
-      await sendMessage(api, { threadID, message: response });
-
-      
-
-      const imageResponse = await axios.get(developer.imageUrl, { responseType: 'stream' });
-
-      
-
-      const msg = {
-        body: '',
-        attachment: imageResponse.data
-      };
-
-      await sendMessage(api, msg, event.threadID); 
+      const imageResponse = await axios.get(developer.imageUrl, { responseType: 'stream' });  
+await sendMessage(api, { threadID, message: response, attachment:  imageResponse.data });
 
     } catch (error) {
       console.error("Error downloading or sending image:", error);
