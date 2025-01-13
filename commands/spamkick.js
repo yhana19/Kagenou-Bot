b370be6cdd5d8751ea6208e50c68026350ad3681
GeoArchonMorax <korisawarezero@gmail.com>
2025-01-13 20:16:24 +0800

    Update spamkick.js

--- commands/spamkick.js
+++ commands/spamkick.js
@@ -9,8 +9,7 @@ module.exports = {
         const { threadID } = event;
 
         try {
-            const jsonData = [{"content":{"url":"https://imgur.com/a/seven-shadows-KKVtWtV.mp4","url_content":"Select ...\nCopy LinkDownload\n1/50\nFull-Sized Dump Of Stolen Memes #006\n86\n6\n3K\ndumpmemesmeme dumpmemefunny\nSelect ...\nCopy LinkDownload\nFirefighters at the Rose Bowl.\n82\n9\n2K\nSelect ...\nCopy LinkDownload\nA cat named Masha…\n107\n18\n6K\n"},"verbose_info":{"file_info":{"name":"file1","url":"https://imgur.com/a/seven-shadows-KKVtWtV.mp4","uri":"file_uri_b79d8525cf55c74b8304dab05661a453","md5":"02ebc89954307667830f710ac31fac13"},"parse_success":true,"total_tokens":78,"read_tokens":78,"meta_file_type":"weblink","meta_page_num":1,"meta_raw_size_kb":0,"meta_parse_size_kb":0}}]; 
-            const imgurMp4Link = jsonData[0].content.url;
+            const imgurMp4Link = 'https://imgur.com/a/seven-shadows-KKVtWtV.mp4'; // Your Imgur MP4 link
 
             const tmpFolderPath = path.join(__dirname, 'tmp');
             if (!fs.existsSync(tmpFolderPath)) {
@@ -43,5 +42,4 @@ module.exports = {
         }
     },
 };
-
-                
+            
